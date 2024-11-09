71fd0b738787003283c74c6781c0ca3af43c5914
ripan baidya <ripanbaidya7980@gmail.com>
2024-11-09 11:48:49 +0530

    demostration of implicit and explicit type casting

---
+++ src/com/ripan/production/basics/TypeCasting.java
@@ -0,0 +1,26 @@
+package com.ripan.production.basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        // implicit
+        int n1 = 10;
+
+        long l1 = n1; // we can directly type caste integer to long
+        double d1 = n1; // and even double, since they have more capacity than int
+
+
+        // explicit
+        long l2 = 10;
+
+        // int n2 = l2; // not possible bcz, int has less capacity than long
+        int n2 = (int) l2;
+
+
+        double d2 = 10.0;
+
+        float f1 = (float) d2;
+
+
+    }
+}
