663938d91389b96b3a638db77bdb495ca7307e25
ripan baidya <ripanbaidya7980@gmail.com>
2024-11-21 12:55:50 +0530

    @Target example

---
+++ src/com/ripan/java/annotation/TargetExample.java
@@ -0,0 +1,38 @@
+package com.ripan.java.annotation;
+
+// @Target is used to define the list of target elements.
+// ElementType -> TYPE, FIELD, CONSTRUCTOR, METHOD
+
+/*
+@Target( value = {ElementType.TYPE,
+				 ElementType.FIELD,
+				 ElementType.CONSTRUCTOR,
+				 ElementType.METHOD})
+@interface Persistable{
+
+}
+
+@Persistable
+public class Employee
+{
+	@Persistable
+	Account account;
+
+	@Persistable
+	Employee(){
+
+	}
+
+	@Persistable
+	public void getEmployeeDetails(){
+
+	}
+}
+
+ */
+
+public class TargetExample {
+    public static void main(String[] args) {
+
+    }
+}
